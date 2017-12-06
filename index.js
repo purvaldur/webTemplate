0f2d91559ab12233a7dde51c88311bd3c3c68e8a
purvaldur <thorvaldha@gmail.com>
2017-12-06 19:15:07 +0100

    Configured "npm start" command

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const app = require('./core/app');
 const http = require('http').Server(app)
 
-http.listen(process.env.PORT || 3000, '0.0.0.0', function(){
-    console.log('listening on purvaldur.dk:3000')
+http.listen(process.env.PORT || 3000, '0.0.0.0', () => {
+    console.log('listening on port 3000')
 })
