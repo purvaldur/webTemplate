e93bfb271706f001061ecb53614edf17de6fabf7
purvaldur <thorvaldha@gmail.com>
2017-12-06 18:49:14 +0100

    Synced local updates

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const app = require('./core/app');
 const http = require('http').Server(app)
 
-console.log(app.get('views'));
-
-http.listen(process.env.PORT || 3002, '0.0.0.0', function(){
-    console.log('listening on purvaldur.dk:3002')
+http.listen(process.env.PORT || 3000, '0.0.0.0', function(){
+    console.log('listening on purvaldur.dk:3000')
 })
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,9 @@
 let vue = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        type: 'app',
+        vueLogo: 'https://upload.wikimedia.org/wikipedia/commons/f/f1/Vue.png',
+        nodeLogo:'https://seeklogo.com/images/N/nodejs-logo-FBE122E377-seeklogo.com.png',
+        author: 'Thorvald Kjartansson'
+    }
 })
